bf7515009793addd3e79bc413b8ac1f64f58f8ee
Alexis GIBERT <agib31@gmail.com>
2024-05-09 23:00:01 +0200

    Update

--- software/Simulation/test.py
+++ software/Simulation/test.py
@@ -1,12 +1,15 @@
-from software.Simulation.Simulation import Simulation
+from Simulation import Simulation
 import numpy as np
 import time
 
 sim = Simulation("UR5_simplified/urdf/UR5_simplified.urdf", startPos=[0,0,0], fixedBase=True, viewMode=False)
+sim.calculate_inverse_kinematics([0,0,0])
 for i in range (6):
     sim.move_joint(i,np.pi/2,1,1000)
     time.sleep(3)
     sim.move_joint(i,-np.pi/2,1,1000)
     time.sleep(3)
+
+
 
 sim.view_mode()
